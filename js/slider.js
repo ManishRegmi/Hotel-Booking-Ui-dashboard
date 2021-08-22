ed1c25c94c3a874fc54bc0518fbb7c2198fe6dec
Manish Regmi <manishregmi39@gmail.com>
2021-08-22 20:46:39 +0545

    changed add hotel file and added switch buttons to it

---
+++ js/slider.js
@@ -0,0 +1,81 @@
+function parkingSlider(){
+ any = document.getElementById('parking-checkbox').checked;
+if(any == true){
+document.getElementById('parkingImage-div').style.display = 'block';
+}else{
+document.getElementById('parkingImage-div').style.display = 'none';
+}
+}
+
+function hallSlider(){
+any = document.getElementById('hall-checkbox').checked;
+if(any == true){
+document.getElementById('check-hall').style.display = 'block';
+}else{
+document.getElementById('check-hall').style.display = 'none';
+}
+}
+
+
+function websiteSlider(){
+any = document.getElementById('website-checkbox').checked;
+if(any == true){
+document.getElementById('hotelorcafe-website').style.display = 'block';
+}else{
+document.getElementById('hotelorcafe-website').style.display = 'none';
+}
+}
+
+function gmailSlider(){
+any = document.getElementById('gmail-checkbox').checked;
+if(any == true){
+document.getElementById('hotelorcafe-gmail').style.display = 'block';
+}else{
+document.getElementById('hotelorcafe-gmail').style.display = 'none';
+}
+}
+
+function facebookSlider(){
+any = document.getElementById('facebook-checkbox').checked;
+if(any == true){
+document.getElementById('hotelorcafe-facebook').style.display = 'block';
+}else{
+document.getElementById('hotelorcafe-facebook').style.display = 'none';
+}
+}
+
+function instaSlider(){
+any = document.getElementById('insta-checkbox').checked;
+if(any == true){
+document.getElementById('hotelorcafe-insta').style.display = 'block';
+}else{
+document.getElementById('hotelorcafe-insta').style.display = 'none';
+}
+}
+
+function youtubeSlider(){
+any = document.getElementById('youtube-checkbox').checked;
+if(any == true){
+document.getElementById('hotelorcafe-youtube').style.display = 'block';
+}else{
+document.getElementById('hotelorcafe-youtube').style.display = 'none';
+}
+}
+
+function tiktokSlider(){
+any = document.getElementById('tiktok-checkbox').checked;
+if(any == true){
+document.getElementById('hotelorcafe-tiktok').style.display = 'block';
+}else{
+document.getElementById('hotelorcafe-tiktok').style.display = 'none';
+}
+}
+
+function tncSlider(){
+any = document.getElementById('tnc-checkbox').checked;
+if(any == true){
+document.getElementById('button').style.display = 'block';
+}else{
+document.getElementById('button').style.display = 'none';
+}
+}
